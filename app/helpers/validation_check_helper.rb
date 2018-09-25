6e5f3eb8d3863cff13375307f42164a39e42a96c
mac-tinoue <tinoue@netprotections.co.jp>
2018-09-25 18:35:00 +0900

    3桁以上の数字が入力された際に1~13の数字を入力するようエラーメッセージを表示

--- app/helpers/validation_check_helper.rb
+++ app/helpers/validation_check_helper.rb
@@ -1,25 +1,25 @@
 module ValidationCheckHelper
-  def validate_cards(c)
+  def validate_cards(cards)
     @error = {}
-    if c !~ /\A([DHSC][1-9]|[DHSC][1][0-3])(\s)([DHSC][1-9]|[DHSC][1][0-3])(\s)([DHSC][1-9]|[DHSC][1][0-3])(\s)([DHSC][1-9]|[DHSC][1][0-3])(\s)([DHSC][1-9]|[DHSC][1][0-3])\z/
+    if cards !~ /\A([DHSC][1-9]|[DHSC][1][0-3])(\s)([DHSC][1-9]|[DHSC][1][0-3])(\s)([DHSC][1-9]|[DHSC][1][0-3])(\s)([DHSC][1-9]|[DHSC][1][0-3])(\s)([DHSC][1-9]|[DHSC][1][0-3])\z/
       msgs = Array.new
-      if c.split.count != 5
+      if cards.split.count != 5
         msg = MessageDefinition::NumberOfCards
         msgs << msg
       end
-      if c.match(/[^DHSC|0-9| ]/).present?
+      if cards.match(/[^DHSC|0-9| ]/).present?
         msg = MessageDefinition::InvalidCharacter
         msgs << msg
       end
-      if c.match(/[DHSC][0]|[DHSC][1][4-9]|[DHSC][2-9][0-9]/).present?
+      if cards.match(/[DHSC][0]|[DHSC][1][4-9]|[DHSC][2-9][0-9]|[DHSC][0-9][0-9][0-9]/).present?
         msg = MessageDefinition::InvalidNumber
         msgs << msg
       end
-      if c.match(/[^\x01-\x7E]/).present?
+      if cards.match(/[^\x01-\x7E]/).present?
         msg = MessageDefinition::EmCharacter
         msgs << msg
       end
-      if c.match(/[0-9][DHSC]/).present?
+      if cards.match(/[0-9][DHSC]/).present?
         msg = MessageDefinition::WithoutHalfSpace
         msgs << msg
       end
@@ -28,12 +28,12 @@ module ValidationCheckHelper
         msgs << msg
       end
       @error = {
-        cards: c,
+        cards: cards,
         error: msgs
       }
-    elsif c.split.size != c.split.uniq.size
+    elsif cards.split.size != cards.split.uniq.size
       @error = {
-        cards: c,
+        cards: cards,
         error: [MessageDefinition::DuplicatedCards]
       }
     end
